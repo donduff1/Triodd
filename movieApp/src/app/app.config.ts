7155b239b8c238facc9926d63eab6c84121f3445
koromierik <112931703+koromierik@users.noreply.github.com>
2024-01-31 16:48:30 +0100

    Delete movieApp directory

--- movieApp/src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { RouterModule } from '@angular/router'; // Importáld be a RouterModule-t
-import { AppRoutingModule } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [AppRoutingModule],
-};
